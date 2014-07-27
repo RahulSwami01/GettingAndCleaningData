6bef011b63a2e514c44c840ee04d6f7dd93a94a6
RahulSwami01 <million.rahul@gmail.com>
2014-07-27 17:17:52 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -26,7 +26,6 @@ ReadDataFile <- function(SuffixFname, PrefixPath) {
     data
 }
 
-mergeData <- function() {
     data <- rbind(ReadTestDataFile(), ReadTrainDataFile())
     cnames <- colnames(data)
     cnames <- gsub("\\.+mean\\.+", cnames, replacement="Mean")
@@ -35,6 +34,8 @@ mergeData <- function() {
     data
 }
 
+
+# Add the activity names as another column
 applyActivityLabel <- function(data) {
     activity_labels <- read.table("activity_labels.txt", header=F, as.is=T, col.names=c("ActivityID", "ActivityName"))
     activity_labels$ActivityName <- as.factor(activity_labels$ActivityName)
@@ -43,15 +44,32 @@ applyActivityLabel <- function(data) {
 }
 
 
-
+# Combine training and test data sets and add the activity label as another column
+getMergedLabeledData <- function() {
     applyActivityLabel(mergeData())
 }
 
 
-
+# Create a tidy data set that has the average of each variable for each activity and each subject.
 getTidyData <- function(merged_labeled_data) {
 if (!require("data.table")) {
   install.packages("data.table")
+}
+
+
+if (!require("reshape2")) {
+  install.packages("reshape2")
+}
+
+    library(reshape2)
+    
+    # melt the dataset
+    id_vars = c("ActivityID", "ActivityName", "SubjectID")
+    measure_vars = setdiff(colnames(merged_labeled_data), id_vars)
+    melted_data <- melt(merged_labeled_data, id=id_vars, measure.vars=measure_vars)
+    
+    # recast 
+    dcast(melted_data, ActivityName + SubjectID ~ variable, mean)    
 }
 
 
